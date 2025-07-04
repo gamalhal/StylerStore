236e938639e8d980edea4cde67ad0f9ac9716ada
Gamal Hal <gamalhal.chi@gmail.com>
2025-07-05 01:16:48 +0300

    Fix: Resolve import path issues for Vercel deployment2"

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": require("path").resolve(__dirname),
+    };
+    return config;
   },
 };
 
